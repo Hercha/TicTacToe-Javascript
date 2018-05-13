d82710338b26610c7589fd0c1ef4f1b6a82c4a65
Jensa1 <jens.hedberg@workaround.io>
2018-05-13 16:36:34 +0200

    setting up variables

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var num, box, ctx, filled, symbol, winner, turn = 1, gameover = false
+    
+    filled = [false, false, false, false, false, false, false, false, false];
+    symbol = ['', '', '', '', '', '', '', '', ''];
+    winner = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+}
