d0f90e086c949451b3beab464c8cf5be82f75cc9
Jensa1 <jens.hedberg@workaround.io>
2018-05-13 20:38:26 +0200

    canvas click function

--- script.js
+++ script.js
@@ -18,4 +18,37 @@ window.onload = function() {
     function newGame() {
         document.location.reload();
     }
+    
+    // 3. Canvas click + retrieving the box's number
+    // canvas click event
+    document.getElementById("tic").addEventListener("click", function(e) {
+        boxClick(e.target.id);
+    });
+    
+    function boxClick(numId) {
+        box = document.getElementById(numId);
+        ctx = box.getContext("2d");
+        
+        switch(numId) {
+            case "canvas1": num = 1;
+                break;
+            case "canvas2": num = 2;
+                break;
+            case "canvas3": num = 3;
+                break;
+            case "canvas4": num = 4;
+                break;
+            case "canvas5": num = 5;
+                break;
+            case "canvas6": num = 6;
+                break;
+            case "canvas7": num = 7;
+                break;
+            case "canvas8": num = 8;
+                break;
+            case "canvas9": num = 9;
+                break;
+        }
+    }
+    
 }
